693bcbcd4090bbbc519005138bb6485bbe75d0f9
Kene Ijeh <keneijeh760@gmail.com>
2026-02-22 12:04:17 +0100

    JWT Security Commit

--- backend/src/main/java/org/example/cavista/controller/PatientController.java
+++ backend/src/main/java/org/example/cavista/controller/PatientController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/patients")
+@RequestMapping("/api/v1/auth/patients")
 public class PatientController {
 
     private final PatientService patientService;
