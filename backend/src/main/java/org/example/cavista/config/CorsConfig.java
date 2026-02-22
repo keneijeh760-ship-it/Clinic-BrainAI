ae1fc3b8f066c122b3e9d2f67f23a875f535206a
Kene Ijeh <keneijeh760@gmail.com>
2026-02-22 11:45:50 +0100

    JWT Security Commit

--- backend/src/main/java/org/example/cavista/config/CorsConfig.java
+++ backend/src/main/java/org/example/cavista/config/CorsConfig.java
@@ -1,4 +1,21 @@
 package org.example.cavista.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
 public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5175")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
