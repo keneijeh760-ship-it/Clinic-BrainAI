3cc58e834ad6d67c3a2aa2333910b828ea5c307b
Kene Ijeh <keneijeh760@gmail.com>
2026-03-15 23:59:10 +0100

    flyway db migration  update

---
+++ backend/src/main/resources/db/migration/V2__create_users_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE app.users (
+    id BIGSERIAL PRIMARY KEY,
+    chew_id VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    phone_number VARCHAR(50),
+    role VARCHAR(50) NOT NULL,
+    password VARCHAR(255)
+);
