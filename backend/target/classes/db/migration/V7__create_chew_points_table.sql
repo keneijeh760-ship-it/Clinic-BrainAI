f4dbb1dfe6001e40c5e540ed674f1ed5eb422d2a
Kene Ijeh <keneijeh760@gmail.com>
2026-04-03 15:28:20 +0100

    remove build artifacts from tracking

--- backend/target/classes/db/migration/V7__create_chew_points_table.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE app.chew_points (
-    id BIGSERIAL PRIMARY KEY,
-    chew_id VARCHAR(255) NOT NULL UNIQUE,
-    total_points INT NOT NULL DEFAULT 0,
-    total_patients_captured INT NOT NULL DEFAULT 0
-);
